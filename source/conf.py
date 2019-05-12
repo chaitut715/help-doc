b00c8e1228fce5699b702ef3b8fedf555a883c8c
Chaitanya Kumar <chaitanya@bilyazilim.com>
2019-05-12 18:04:16 +0530

    added master_doc conf

--- source/conf.py
+++ source/conf.py
@@ -23,7 +23,7 @@ author = 'Hakan Moral'
 
 # The full version, including alpha/beta/rc tags
 release = '0.0.1'
-
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
